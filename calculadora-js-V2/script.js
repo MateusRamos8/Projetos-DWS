19f71048b8de3bcec5320d68db480ba62e98e104
Mateus Ramos <mateusramoscolognesi@gmail.com>
2024-03-16 14:32:06 -0300

    calc-js-V2_Commit 3.0
    quase tudo pronto, responsivo pra celular, só falta o botão de histórico

--- calculadora-js-V2/script.js
+++ calculadora-js-V2/script.js
@@ -1,7 +1,7 @@
 let menuVisible = false;
+let menu = document.getElementById("menu");
+
 document.getElementById("menu-btn").addEventListener("click", ()=>{
-  let menu = document.getElementById("menu");
-  
   if(!menuVisible){
     menu.style.transform = "translateX(0%)";
     //s_fix_scrl.style.display = "block";
@@ -12,4 +12,7 @@ document.getElementById("menu-btn").addEventListener("click", ()=>{
     menuVisible = false;
   }
 })
+
+
+
 
