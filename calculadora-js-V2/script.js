ea83e4545f9dd2061171169fe1b213d468a779e8
Mateus Ramos <mateusramoscolognesi@gmail.com>
2024-03-24 22:20:56 -0300

    calc-js-V2_Commit 3.1
    Arrumando o #hist

--- calculadora-js-V2/script.js
+++ calculadora-js-V2/script.js
@@ -1,17 +1,34 @@
 let menuVisible = false;
+let histVisible = false;
+let body = document.getElementsByTagName("body")[0];
 let menu = document.getElementById("menu");
+let hist = document.getElementById("hist");
 
 document.getElementById("menu-btn").addEventListener("click", ()=>{
   if(!menuVisible){
     menu.style.transform = "translateX(0%)";
-    //s_fix_scrl.style.display = "block";
     menuVisible = true;
   } else{
     menu.style.transform = "translateX(-150%)";
-    //s_fix_scrl.style.display = "none";
     menuVisible = false;
   }
 })
+document.getElementById("hist-btn").addEventListener("click", ()=>{
+  if(!histVisible){
+    hist.style.transform = "translateY(0%)";
+    histVisible = true;
+  } else{
+    hist.style.transform = "translateY(150%)";
+    histVisible = false;
+  }
+})
+window.matchMedia("(min-width: 556px)").addEventListener("change",(e) =>{
+  if (e.matches) {
+    hist.style.transform = "translateY(150%)";
+    hist.style.display = "none"
+    histVisible = false;
+  }
+});
 
 
 
